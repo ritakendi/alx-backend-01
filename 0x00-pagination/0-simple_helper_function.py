c455ec533b14693c552194a9a1552aede2ac4f19
Kendi Rita <ritabkendi@gmail.com-MacBook-Pro.local>
2023-01-23 19:26:09 +0300

    added type-annotation

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> tuple:
     """
-        This function takes in two arguments, page and page_size,
-        which are used to calculate the start and end indexes for the range of items to return in a list. 
+        This function takes in two arguments, 
+                page and page_size,
+        which are used to calculate 
+                the start and end indexes,
+         for the range of items to return in a list. 
         The page argument represents the current page number
         while the page_size argument represents the number of items to return per page.
     """
